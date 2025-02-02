9f54b15e98a24da8325f03f906adfb46197f61b5
vanetti21 <jatnnalieberherr24@gmail.com>
2025-02-02 17:37:27 -0400

    Layaout

--- haven-match/app/layout.tsx
+++ haven-match/app/layout.tsx
@@ -29,7 +29,6 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <Navbar />
-        {children}
       </body>
     </html>
   );
