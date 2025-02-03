140c782d78893ba2ad819d7d3a3537588dc8d1ff
vanetti21 <jatnnalieberherr24@gmail.com>
2025-02-03 05:47:47 -0400

    MenuItem y más Commit

--- haven-match/app/components/Navbar/Navbar.tsx
+++ haven-match/app/components/Navbar/Navbar.tsx
@@ -1,5 +1,9 @@
+'use client';
+
 import Container from "../Container";
 import Logo from "./Logo";
+import Search from "./Search";
+import UserMenu from "./UserMenu";
 
 const Navbar = () => {
     return (
@@ -21,8 +25,9 @@ const Navbar = () => {
                         "
                     >
                         
-                        <Logo/>
-                        
+                    <Logo/>
+                    <Search/>
+                    <UserMenu/>
 
                     </div>
                 </Container>
